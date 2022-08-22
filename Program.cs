5ecae4714dd804f5764ef813a17b0ad339b87641
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-08-22 22:54:07 +0300

    Null using added

--- Program.cs
+++ Program.cs
@@ -17,19 +17,19 @@ void PrintMatrix(int[,] matrix)
     }
     
 }
-int[] FindPosition(int[,] matrix, int value)
+int[]? FindPosition(int[,] matrix, int value)
 {
     for(int i=0;i<matrix.GetLength(0);i++)
         for(int j=0;j<matrix.GetLength(1);j++)
             if(matrix[i,j]==value)
                 return new int[]{i,j};
-    return new int[] {-1,-1};
+    return null;
 }
 int[,] matrix = CreateMatrix(3,3,0,20);
 PrintMatrix(matrix);
 Console.Write("Enter element to find: ");
 int value = int.Parse(Console.ReadLine() ?? "0");
-int[] position = FindPosition(matrix,value);
-Console.WriteLine(position[0]< 0 || position[1]< 0 
+int[]? position = FindPosition(matrix,value);
+Console.WriteLine(position==null
     ? "Element not found" 
     : $"Position of element: ({position[0]},{position[1]})");
